749b441264104a73681bed72d896509cdcb4c8f8
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2022-02-10 22:47:07 +0100

    handled authentication in session controller and added helper methods in application controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+  def create
+    user = user.find_by(email: params[:email])
+    if user
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect to user_path(user)
+      else
+        flash.now.alert = 'Incorrect password, please try again.'
+        redirect to '/', flash: { alert: alert }
+      end
+    else
+      flash.now.alert 'Email not found , please try again.'
+      redirect to '/', flash: { alert: alert}
+    end
+  end
 end
