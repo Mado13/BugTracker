80d488f3cdc061a4666a9886b964613da112c664
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2022-02-13 18:23:17 +0100

    styling sign up/sign in/forgot password pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,10 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  ActionView::Base.field_error_proc = proc do |html_tag, _instance|
+    html_tag.html_safe
+  end
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
