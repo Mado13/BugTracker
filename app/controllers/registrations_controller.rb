59a6e71fa01f4b1eb720f7e0599e890e5edc7c7b
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2022-02-13 12:25:17 +0100

    added tickets and projects controllers

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(_resource)
-    profile_path_url
+    user_path(current_user)
   end
 end
--- app/controllers/sessions_controller.rb
+++
@@ -1,7 +0,0 @@
-class SessionsController < Devise::SessionsController
-  protected
-
-  def after_sign_in_path_for(_resource)
-    profile_path
-  end
-end
