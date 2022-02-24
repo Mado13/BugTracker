c433f9c64e9b9d3317546d5511bfbde391d2918d
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2022-02-24 14:44:22 +0100

    refactored user model to have a dynamic scope and a custom join to count projects by roles

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,8 +5,8 @@ class DashboardController < ApplicationController
       @tickets_by_priority = Ticket.tickets_counts('priority')
       @tickets_by_status = Ticket.tickets_counts('status')
       @tickets_by_developer = User.tickets_by_developer
-      @projects_by_lead_developer = User.projects_by_lead_developer
-      @projects_by_project_manager = User.projects_by_project_manager
+      @projects_by_lead_developer = User.count_projects('lead_developer')
+      @projects_by_project_manager = User.count_projects('project_manager')
     else
       redirect_to root_path
       falsh[:notice] = 'You are not authorized to preform that action'
