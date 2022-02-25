b81a926f40e71282b997e56e1fff6f46748ad446
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2022-02-25 12:53:43 +0100

    turebo links redundent

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -4,11 +4,9 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import '../js/bootstrap_js_files.js'
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
