fda8ac5f34a6f0e7e53b548e9df6e7dd57283c06
Matan Amidor <87947521+Mado13@users.noreply.github.com>
2022-02-28 09:13:16 +0100

    project correction

--- app/presenters/project_presenter.rb
+++ app/presenters/project_presenter.rb
@@ -14,12 +14,12 @@ class ProjectPresenter
 
   # Renders Project Manager Select_Tag for new project form if the user is admin.
   def admin_select_tag
-    return if @user.admin?
-
-    h.select_tag 'project[project_manager_id]',
+    if @user.admin?
+      h.select_tag 'project[project_manager_id]',
                  h.options_from_collection_for_select(User.users_by_role('Project Manager'), :id, :email),
                  class: 'form-control col-md-4 col-form-label text-md-right',
                  prompt: 'Select Project Manager'
+    end
   end
 
   # render hidden_field_tag for new project form if the user creating the
